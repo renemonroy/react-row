f80d18a9b6b20e8f209df064afbf789caa7e9678
René Monroy <rene.monroy@gmail.com>
2015-04-23 10:25:13 -0500

    Build the jsx index source

---
+++ example/js/init.jsx
@@ -0,0 +1,9 @@
+'use strict';
+
+import React from 'react/addons';
+import App from './app';
+
+let appWrapperEl = document.getElementById('app-wrapper'),
+  loadApp = e => React.render(<App />, appWrapperEl);
+  
+document.addEventListener('DOMContentLoaded', loadApp);
