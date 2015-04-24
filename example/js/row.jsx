8d31592cdc19477a5ba2a4ec84eed34e8c5c336a
René Monroy <rene.monroy@gmail.com>
2015-04-24 11:19:13 -0500

    Add resize behavior

--- example/js/row.jsx
+++ example/js/row.jsx
@@ -11,29 +11,34 @@ import Draggable from 'react-draggable';
  */
 let Column = React.createClass({
   displayName : 'Column',
+
+  /**
+   * Props can't be updated from a owner component, but state can be. The
+   * state is changed by Row component only on those columns that need it.
+   * These 2 columns will re render themselves with these states.
+   */
   getInitialState() {
     var ps = this.props;
-    return { width : ps.initialWidth ? parseInt(ps.initialWidth) : null };
+    return { colWidth : ps.colWidth ? parseInt(ps.colWidth) : null }
   },
-  renderChild() {
-    var col = this;
-    return React.Children.map(this.props.children, (child) => {
-      if ( col.props.initialWidth ) {
-        return React.cloneElement(child, { colWidth : col.state.width + 'px' });
-      }
-      return child;
-    });
-  },
+
+  /**
+   * Just renders the column wrapper of each element inside a container, if
+   * the width of the col is not setup or equals to 0 the flex width will
+   * be 1 without any width in pixels.
+   */
   render() {
     var st = this.state,
-      styles = { flex : st.width ? '0 1 ' + st.width + 'px' : '1' };
+      flex = st.colWidth && (st.colWidth > 0) ? '0 1 ' + st.colWidth + 'px' : '1',
+      styles = { flex : flex };
     return (
       <div {...this.props} style={styles} className="column">
-        { this.renderChild() }
+        { this.props.children }
       </div>
     );
   }
 });
+
 
 /**
  * Row wraps children into columns to control layouts with the help of flex
@@ -41,24 +46,66 @@ let Column = React.createClass({
  */
 let Row = React.createClass({
   displayName : 'Row',
-  getInitialState() {
-    var ps = this.props;
-    return { resizable : ps.resizable };
+
+  /**
+   * Sets initial variables, which is only 'resizable' for now. 
+   */
+  getDefaultProps() {
+    return { rowName : null, resizable : true };
   },
+
+  /**
+   * Builds columns with cached widths and possible draggables if configured 
+   * as true via props.
+   */
   renderColumns(comps) {
     var row = this, cols = [],
       rowName = row.props.rowName ? row.props.rowName + '-' : '';
     comps.forEach( (comp, i) => {
       var colName = rowName + 'col-' + i,
-        colWidth = comp.props.colWidth || null;
+        cachedWidth = localStorage.getItem(colName),
+        colWidth = (cachedWidth !== null) ? parseInt(cachedWidth, 10) : (comp.props.colWidth || null);
+      if ( (i > 0) && row.props.resizable ) {
+        cols.push(
+          <Draggable key={ colName + 'handler-' + i } axis='x' zIndex={1} start={{ x : 0, y: 0 }} onDrag={row._onHandlerDrag.bind(row, i)}>
+            <div><div className="handler-icon"></div></div>
+          </Draggable>
+        );
+      }
       cols.push(
-        <Column key={ colName } ref={ colName } initialWidth={ colWidth } >
+        <Column key={ colName } ref={ colName } colWidth={ colWidth } >
           { comp }
         </Column>
       );
     });
     return cols;
   },
+
+  /**
+   * When the draggable is handled to resize, it gets the current and previous
+   * columns involved data to change its dimensions.
+   */
+  _onHandlerDrag(i, e, ui) {
+    var rfs = this.refs, ps = this.props,
+      rowName = ps.rowName ? ps.rowName + '-' : '',
+      rColName = rowName + 'col-' + i,
+      lColName = rowName + 'col-' + (i - 1),
+      rCol = rfs[rColName],
+      lCol = rfs[lColName],
+      rColW = (rCol.state.colWidth || parseInt(rCol.getDOMNode().offsetWidth)) - e.movementX,
+      lColW = (lCol.state.colWidth || parseInt(lCol.getDOMNode().offsetWidth)) + e.movementX;
+    
+    rCol.setState({ colWidth : rColW });
+    lCol.setState({ colWidth : lColW });
+
+    if ( ps.rowName && rCol.state.colWidth ) localStorage.setItem(rColName, rColW.toString());
+    if ( ps.rowName && lCol.state.colWidth ) localStorage.setItem(lColName, lColW.toString());
+  },
+
+  /**
+   * Renders the row element into a owner component. If it doesn't get any
+   * child it will be an simple empty container.
+   */
   render() {
     var comps = this.props.children;
     return (
